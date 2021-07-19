59faa24e7f1c4761df572e9ccfe2269e0f7e8a86
cyclopssound <cyclopssound@gmail.com>
2021-07-19 15:14:34 -0700

    Scene Builder testing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public Label TheLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Initialized");
+    }
+
+    public void OnButtonAction(ActionEvent actionEvent) {
+        System.out.println("Click!");
+        TheLabel.setText("Ya Clicked It");
+    }
 }
