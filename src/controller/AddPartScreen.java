bfe404d2cab186ad867d78a8127a221c6e0605e1
cyclopssound <cyclopssound@gmail.com>
2021-07-19 17:40:53 -0700

    Add/Modify Part screen layouts

--- src/controller/AddPartScreen.java
+++ src/controller/AddPartScreen.java
@@ -1,4 +1,20 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class AddPartScreen {
+    public RadioButton InhouseRadio;
+    public RadioButton OutsourcedRadio;
+    public Label MachineCompanyLabel;
+    public TextField NameField;
+    public TextField InvField;
+    public TextField PricecostField;
+    public TextField MinValue;
+    public TextField MachineCompanyField;
+    public TextField MaxField;
+    public Button SaveButton;
+    public Button CancelButton;
 }
--- src/controller/MainScreen.java
+++ src/controller/MainScreen.java
@@ -7,19 +7,15 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 public class MainScreen {
-    public Pane PartsPane;
     public TableView PartsTable;
-    public Label PartsLabel;
     public TextField PartsSearch;
     public Button AddPartButton;
     public Button ModifyPartButton;
     public Button DeletePartButton;
-    public Pane ProductsPane;
     public TableView ProductsTable;
-    public Label ProductsLabel;
     public TextField ProductsSearch;
     public Button AddProductButton;
     public Button ModifyPartButton1;
     public Button DeletePartButton1;
-    public Label ProgramLabel;
+    public Button ExitButton;
 }
