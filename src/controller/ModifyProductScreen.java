e3e0876a16323dc8f46cf7b37ff5ac7aa86fa35f
cyclopssound <cyclopssound@gmail.com>
2021-07-19 18:46:01 -0700

    Add/Modify Product screen layouts

--- src/controller/ModifyProductScreen.java
+++ src/controller/ModifyProductScreen.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ModifyProductScreen {
+    public TextField PartSearch;
+    public TextField IdField;
+    public TextField NameField;
+    public TextField InvField;
+    public TextField PriceField;
+    public TextField MaxField;
+    public TextField MinField;
+    public TableView PartTable;
+    public Button AddButton;
+    public TableView AssociatedPartTable;
+    public Button RemoveAssociatedButton;
+    public Button SaveButton;
+    public Button CancelButton;
 }
